48d49c134079ed097e906178999d01ce58a1499f
Alexandre Bulté <alexandre@bulte.net>
2012-09-08 10:01:25 +0200

    Config file for db url and serial device

---
+++ config.js
@@ -0,0 +1,2 @@
+exports.db_url = "./temps.db";
+exports.serial_device = "/dev/ttyACM0";
--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,11 @@ db = require('../lib/db');
 
 exports.index = function(req, res){
   var data = [];
-  // db.each("SELECT ts, temp FROM temps", function(err, row) {
-  //       if(err) { console.log(err); }
-  //       else{
-  //           data.push([row.ts, row.temp]);
-  //       }
-  // });
+  db.each("SELECT ts, temp FROM temps", function(err, row) {
+        if(err) { console.log(err); }
+        else{
+            data.push([row.ts, row.temp]);
+        }
+  });
   res.render('index', { title: 'Températures', data: data });
 };
